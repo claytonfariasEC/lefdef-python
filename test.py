02331df41cc1733e5bf7b86e1645cf2653a09fd0
clayton farias <claytonsk2012@gmail.com>
2025-02-16 17:02:57 -0300

    UPdate project

--- test.py
+++ test.py
@@ -7,3 +7,11 @@ from lefdef import C_DefReader
 def_reader = C_DefReader()
 _def = def_reader.read(".\datapath.def")
 _def.print()
+
+
+#COMO MANIPULAR ISSO
+lista = _def.c_g_cell_grid_x
+
+
+#for i in lista:
+#    print(i)
